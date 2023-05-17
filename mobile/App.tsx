6547c7ead60bc9843f414214dd3864b80c400321
Daniel Filipe Schwingel <daniel.schwingel@intelligenz.tech>
2023-05-17 16:52:28 -0300

    First class

---
+++ mobile/App.tsx
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View className='bg-slate-900 flex-1'>
+      <Text>Hello world!</Text>
+      <StatusBar style="light" />
+    </View>
+  );
+}
+
