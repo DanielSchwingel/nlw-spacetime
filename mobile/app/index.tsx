dbdb47c68103f6254d8c79544276c714a45a0221
Daniel Filipe Schwingel <daniel.schwingel@intelligenz.tech>
2023-05-18 16:14:17 -0300

    Third class

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,22 +1,62 @@
 import { StatusBar } from 'expo-status-bar'
 import { ImageBackground, View, Text, TouchableOpacity } from 'react-native'
 import { styled } from 'nativewind'
-
+import { useEffect } from 'react'
+import { useAuthRequest, makeRedirectUri } from 'expo-auth-session'
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
 import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
+import * as SecureStore from 'expo-secure-store'
+import { useRouter } from 'expo-router'
 
-import blurBg from './src/assets/bg-blur.png'
-import Stripes from './src/assets/stripes.svg'
-import NLWLogo from './src/assets/nlw-spacetime-logo.svg'
+import blurBg from '../src/assets/bg-blur.png'
+import Stripes from '../src/assets/stripes.svg'
+import NLWLogo from '../src/assets/nlw-spacetime-logo.svg'
+import { api } from '../src/lib/api'
 
 const StyledStripes = styled(Stripes)
 
+const discovery = {
+	authorizationEndpoint: 'https://github.com/login/oauth/authorize',
+	tokenEndpoint: 'https://github.com/login/oauth/access_token',
+	revocationEndpoint: 'https://github.com/settings/connections/applications/9a0e914f30cc89ba1989'
+}
+
 export default function App() {
+   const router = useRouter()
 	const [ hasLoadedFonts ] = useFonts({
 		Roboto_400Regular,
 		Roboto_700Bold,
 		BaiJamjuree_700Bold
 	})
+	const [, response, signInWithGithub ] = useAuthRequest(
+		{
+		  clientId: '9a0e914f30cc89ba1989',
+		  scopes: ['identity'],
+		  redirectUri: makeRedirectUri({
+			 scheme: 'nlwspacetime'
+		  })
+		},
+		discovery
+	);
+
+   async function handleGithubOAuthCode(code: string) {
+      const response = await api.post('/register', {
+         code
+      })
+      
+      const { token } = response.data
+
+      await SecureStore.setItemAsync('token', token)
+
+      router.push('/memories')
+   }
+
+	useEffect(() => {
+		if (response?.type === 'success') {
+			const { code } = response.params
+         handleGithubOAuthCode(code)
+		}
+	}, [response])
 
 	if (!hasLoadedFonts) {
 		return null
@@ -42,7 +82,11 @@ export default function App() {
 						Colecione momentos marcantes da sua jornada e compartilhe (se quiser) com o mundo!
 					</Text>
 				</View>
-				<TouchableOpacity className='rounded-full bg-green-500 px-5 py-2' activeOpacity={0.7}>
+				<TouchableOpacity 
+					className='rounded-full bg-green-500 px-5 py-2' 
+					activeOpacity={0.7}
+					onPress={() => signInWithGithub()}
+				>
 					<Text className=' font-alt text-sm uppercase text-black'>
 						CADASTRAR LEMBRANÇA
 					</Text>
--- server/src/routes/memories.ts
+++ server/src/routes/memories.ts
@@ -2,12 +2,20 @@ import { FastifyInstance } from 'fastify'
 import { z } from 'zod'
 
 import { prisma } from '../lib/prisma'
+import { request } from 'http'
 
 export async function memoriesRoutes(app: FastifyInstance ) {
 
-   app.get('/memories', async () => {
-      
-      const memories = await prisma.memory.findMany({
+	app.addHook('preHandler', async (request) => {
+		await request.jwtVerify()
+	})
+
+   app.get('/memories', async (request) => {
+
+		const memories = await prisma.memory.findMany({
+			where: {
+				userId: request.user.sub
+			},
          orderBy: {
             createdAT: 'asc'
          }
@@ -22,7 +30,7 @@ export async function memoriesRoutes(app: FastifyInstance ) {
       })
    })
    
-   app.get('/memories/:id', async (request) => {
+   app.get('/memories/:id', async (request, reply) => {
       const paramsSchema = z.object({
 			id: z.string().uuid()
 		})
@@ -34,6 +42,10 @@ export async function memoriesRoutes(app: FastifyInstance ) {
 				id
 			}
 		})
+
+		if (!memory.isPublic && memory.userId !== request.user.sub) {
+			return reply.status(401).send()
+		}
 
 		return memory
    })
@@ -52,7 +64,7 @@ export async function memoriesRoutes(app: FastifyInstance ) {
 				content,
 				coverUrl,
 				isPublic,
-				userId: "e70374b5-d175-4a72-b602-263ee23d3564"
+				userId: request.user.sub
 			}
 		})
 
@@ -60,7 +72,7 @@ export async function memoriesRoutes(app: FastifyInstance ) {
 
    })
 
-   app.put('/memories/:id', async (request) => {
+   app.put('/memories/:id', async (request, reply) => {
       const paramsSchema = z.object({
 			id: z.string().uuid()
 		})
@@ -75,7 +87,17 @@ export async function memoriesRoutes(app: FastifyInstance ) {
 
 		const { content, coverUrl, isPublic } = bodySchema.parse(request.body)
 
-		const memory = await prisma.memory.update({
+		let memory = await prisma.memory.findUniqueOrThrow({
+			where: {
+				id
+			}
+		})
+
+		if (memory.userId == request.user.sub) {
+			return reply.status(401).send()
+		}
+
+		memory = await prisma.memory.update({
 			where: {
 				id
 			},
@@ -89,12 +111,22 @@ export async function memoriesRoutes(app: FastifyInstance ) {
 		return memory
    })
 
-   app.delete('/memories/:id', async (request) => {
+   app.delete('/memories/:id', async (request, reply) => {
       const paramsSchema = z.object({
 			id: z.string().uuid()
 		})
 
 		const { id } = paramsSchema.parse(request.params);
+
+		const memory = await prisma.memory.findUniqueOrThrow({
+			where: {
+				id
+			}
+		})
+
+		if (memory.userId == request.user.sub) {
+			return reply.status(401).send()
+		}
 
 		await prisma.memory.delete({
 			where: {
